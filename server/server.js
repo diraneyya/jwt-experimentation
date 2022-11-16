9fb6434245cb3d4a9f220df4dec5b83f4bbc0e33
Orwa Diraneyya <diraneyya@wildcodeschool.com>
2022-11-16 13:58:22 +0100

    Added cookie support

--- server/server.js
+++ server/server.js
@@ -4,12 +4,16 @@ import { readFile } from "node:fs/promises";
 import path from "path";
 import Fastify from "fastify";
 import jwt from "@fastify/jwt";
+import cookie from "@fastify/cookie";
 
 const fastify = Fastify({
   logger: true,
 });
 
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+// register cookie plugin
+fastify.register(cookie, {});
 
 // secret as an object of RSA keys (without passphrase)
 // the files are loaded as strings
@@ -19,6 +23,10 @@ fastify.register(jwt, {
     public: readFileSync(path.join(__dirname, "/public.key"), "utf8"),
   },
   sign: { algorithm: "RS256" },
+  cookie: {
+    cookieName: "token",
+    signed: false,
+  },
 });
 
 fastify.get("/public", async function (request, reply) {
@@ -51,19 +59,35 @@ fastify.get("/signin", async function (request, reply) {
     signedIn: true,
     name: request.query.name,
   });
+  reply.setCookie("token", token, {
+    //   domain: 'localhost:3000',
+    //   path: '/',
+    //   secure: false, // send cookie over HTTPS only
+    //   httpOnly: true,
+    //   sameSite: true // alternative CSRF protection
+  });
+
   return `
-    <h1>Instructions</h1>
+    <h1>Manual Instructions</h1>
     <p>In order to access the signedin route '/signedin', you will need to use Postman
     or a similar API tester to use the GET http verb and while passing the following
     HTTP header to the server:</p>
     <code>Authentication: Bearer &lt;token&gt;</code>
     <h2>Token</h2>
-    <textarea style="width: 100%; height: 30%; overflow-y: scroll"
+    <textarea style="width: 100%; height: 25%; overflow-y: scroll"
     >${token}</textarea>
+    <h1>Using Cookies</h1>
+    <p>Instead of doing the above, we can also use the cookies in the browser. Using
+    Chrome check the cookie called "token" which was created. After that go to the
+    <a href="/signedin">signedin route "/signed"</a> on the server to see the login
+    message.</p>
+    <p>After that, try to clear the cookie and do the same, again.</p>
   `;
 });
 
 fastify.get("/signedin", async function (request, reply) {
+  console.log(request.cookies);
+
   reply.type("text/html");
   try {
     const loginData = await request.jwtVerify();
@@ -74,9 +98,13 @@ fastify.get("/signedin", async function (request, reply) {
   } catch (error) {
     return `
             <h1>You are not signed in!</h1>
-            <p>Did you remember to send an Authentication header to this route? check the
-            signin route '/signin' for instructions.</p>
-            <p> The error message given by the JWT fastify library is "<em>${error}</em>"</p>
+            <p>Did you forget to sign in using the signin route "/signin", or, did you clear
+            the cookie manually?</p>
+            <p>If using Postman or Thunder Client, did you remember to send an Authentication
+            header to this route? check the <a href="signin">signin route '/signin'</a> for 
+            instructions.</p>
+            <p> The error message given by the JWT fastify library is:</p>
+            <blockquote>"<em>${error}</em>"<blockquote>
         `;
   }
 });
